36c7717ae6384480318b96cc72455ff1254ef430
jahzeelnavalta19 <jahzeel.navalta@deped.gov.ph>
2026-07-06 22:14:57 +0800

    Fix manifest, add caching service worker, add screenshot

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,46 @@
-self.addEventListener('install',(e)=>{self.skipWaiting();});
-self.addEventListener('activate',(e)=>{e.waitUntil(clients.claim());});
-self.addEventListener('fetch',(e)=>{e.respondWith(fetch(e.request));});
+const CACHE_NAME = 'virtual-quiz-v2';
+const urlsToCache = [
+  '/',
+  '/quiz.html',
+  '/kahoot-player.html',
+  '/kahoot-host.html',
+  '/dashboard.html',
+  '/style.css',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png',
+  '/socket.io/socket.io.js'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((names) => {
+      return Promise.all(
+        names.filter((name) => name !== CACHE_NAME).map((name) => caches.delete(name))
+      );
+    })
+  );
+  e.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((resp) => {
+      return resp || fetch(e.request).then((response) => {
+        return caches.open(CACHE_NAME).then((cache) => {
+          cache.put(e.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
